3584907ca5b16f967bb3a01261a1796a8e48a3e0
Ben Farrell <ben@benfarrell.com>
2024-12-21 19:54:39 -0800

    v0.1.2 (2024-12-08) - Add util in baseplayback to cancel loop - Add clone, trim actions and UUID property in MIDITrack

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+### v0.1.3 (2024-12-21)
+- Finally needed file handler processing (vs the URL) to load MIDI files, so made MidiFile.load accept URI or files
 
 ### v0.1.2 (2024-12-08)
 - Add util in baseplayback to cancel loop
--- src/utils/midifile.ts
+++ src/utils/midifile.ts
@@ -25,34 +25,50 @@ export class MIDIFile {
 
     public timeMeta: TimeMeta = {};
 
-    static Load(uri: string): Promise<MIDIFile> {
-        return new Promise((resolve) => {
-            fetch(uri).then((response) => {
-                response.arrayBuffer().then((buffer) => {
-                    parseArrayBuffer(buffer).then((json: IMidiFile) => {
-                        const midi = new MIDIFile();
-                        midi.data = json;
-                        midi.name = uri;
-                        midi.timeMeta.division = json.division;
+    static Load(uri: string | File): Promise<MIDIFile> {
+        const midireader = (buffer: ArrayBuffer, name: string): Promise<MIDIFile> => {
+            return new Promise((resolve, _reject) => {
+                parseArrayBuffer(buffer).then((json: IMidiFile) => {
+                    const midi = new MIDIFile();
+                    midi.data = json;
+                    midi.name = name;
+                    midi.timeMeta.division = json.division;
 
-                        midi.data.tracks.forEach((_track, i) => {
-                            const track = midi.parseTrack(i);
-                            if (track?.events.length === 0) {
-                                if (i !== 0 && track.hasTimingInfo) console.warn('Timing information is coming from a track that is not the first track');
-                                // this might be a timing track, record the info and don't add it to the list
-                                if (track.timeSignature && track.hasTimingInfo) midi.timeMeta.timeSignature = track.timeSignature;
-                                if (track.tempo && track.hasTimingInfo) midi.timeMeta.tempo = track.tempo;
-                            } else if (track) midi.tracks.push(track);
-                        });
-
-                        midi.tracks.forEach((track) => {
-                            track.populateMissingTimeData(midi.timeMeta);
-                        });
-
-                        resolve(midi);
+                    midi.data.tracks.forEach((_track, i) => {
+                        const track = midi.parseTrack(i);
+                        if (track?.events.length === 0) {
+                            if (i !== 0 && track.hasTimingInfo) console.warn('Timing information is coming from a track that is not the first track');
+                            // this might be a timing track, record the info and don't add it to the list
+                            if (track.timeSignature && track.hasTimingInfo) midi.timeMeta.timeSignature = track.timeSignature;
+                            if (track.tempo && track.hasTimingInfo) midi.timeMeta.tempo = track.tempo;
+                        } else if (track) midi.tracks.push(track);
                     });
+
+                    midi.tracks.forEach((track) => {
+                        track.populateMissingTimeData(midi.timeMeta);
+                    });
+
+                    resolve(midi);
                 });
             });
+        }
+
+        return new Promise((resolve, _reject) => {
+            if (typeof uri === 'string') {
+                fetch(uri).then((response) => {
+                    response.arrayBuffer().then((buffer) => {
+                        resolve(midireader(buffer, uri));
+                    });
+                });
+            } else {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    if (e.target?.result instanceof ArrayBuffer) {
+                        resolve(midireader(e.target.result, uri.name));
+                    }
+                };
+                reader.readAsArrayBuffer(uri);
+            }
         });
     }
 
